9a276946936d9952a22aaefa90d3a7a7dbd17ecd
ottav14 <dominicjottaviano@gmail.com>
2025-03-12 04:35:21 -0400

    We're energy stable babygit add .git add .

--- src/main.js
+++ src/main.js
@@ -2,16 +2,17 @@ import './style.css';
 import * as LA from './la.js';
 import { hue, clamp } from './util.js';
 
-const canvasWidth = 800;
-const canvasHeight = canvasWidth;
-const circleCount = 10;
+const canvasWidth = 1000;
+const canvasHeight = 800;
+const circleCount = 80;
+const eulerIterations = 4;
 const spawnAttempts = 500;
 const circleSizeFactor = 30;
 const mass = 1;
-const g = 0.3;
+const g = 0.0;
 const drag = 0.05;
-const maxVelocity = 8;
-const initialVelocity = 5;
+const maxVelocity = 9;
+const initialVelocity = 9;
 const energyLoss = 0.3;
 const backgroundColor = '#000'
 const friction = 0.00;
@@ -29,22 +30,10 @@ const createCircles = () => {
 	for(let i=0; i<circleCount; i++) {
 		const radius = mass * circleSizeFactor;
 
-		let position = {
-			x: Math.floor((canvasWidth-2*radius)*Math.random()) + radius,
-			y: Math.floor((canvasHeight-2*radius)*Math.random()) + radius,
-		}
-			
-		for(let i=0; i<spawnAttempts; i++) {
-			for(const other of newCircles) {
-				if(LA.distance(position, other) > radius+other.r)
-					break;
-				else {
-					position = {
-						x: Math.floor((canvasWidth-2*radius)*Math.random()) + radius,
-						y: Math.floor((canvasHeight-2*radius)*Math.random()) + radius,
-					}
-				}
-			}
+		const cpr = 10;
+		const position = {
+			x: (i%cpr)*3*radius + 3*radius,
+			y: Math.floor(i/cpr)*radius*3 + 3*radius
 		}
 
 		newCircles.push({
@@ -91,74 +80,66 @@ const drawLine = (x1, y1, x2, y2) => {
 	ctx.stroke();
 }
 
-const step = (p) => {
-	p.vx = clamp(p.vx, -maxVelocity, maxVelocity);
-	p.vy = clamp(p.vy, -maxVelocity, maxVelocity);
-	p.x += p.vx;
-	p.y += p.vy;
+const step = (c, t) => {
+	c.vy += t*t*g;
+	c.x += t*c.vx;
+	c.y += t*c.vy;
 }
 
-const undoStep = (p) => {
-	p.x -= p.vx;
-	p.y -= p.vy;
+const undoStep = (c, t) => {
+	c.vy -= t*t*g;
+	c.x -= t*c.vx;
+	c.y -= t*c.vy;
 }
 
-
-
-
-
-const handleCollision = (c1, c2) => {
+const handleCollision2 = (c1, c2) => {
 
 	const v1 = { x: c1.vx, y: c1.vy };
 	const v2 = { x: c2.vx, y: c2.vy };
+	const n = LA.normalize(LA.sub(v2, v1));
+	const t = { x: n.y, y: -n.x };
+	
+	const vn1 = LA.dot(v1, n);
+	const vt1 = LA.dot(v1, t);
+	const vn2 = LA.dot(v2, n);
+	const vt2 = LA.dot(v2, t);
 
-	const m1 = mass;
-	const m2 = mass;
+	const v1d = ((c1.m-c2.m) * vn1 + 2*c2.m*vn2) / (c1.m+c2.m);
+	const v2d = ((c2.m-c1.m) * vn2 + 2*c1.m*vn1) / (c1.m+c2.m);
 
-	const rv1 = LA.sub(v1, v2);
-	const rv2 = LA.sub(v2, v1);
+	c1.vx = v1d * n.x + vt1 * t.x;
+	c1.vy = v1d * n.y + vt1 * t.y;
+	c2.vx = v2d * n.x + vt2 * t.x;
+	c2.vy = v2d * n.y + vt2 * t.y;
 
-	const rp1 = LA.sub(c1, c2);
-	const rp2 = LA.sub(c2, c1);
-
-	const d = LA.distance(c1, c2);
-
-	const v1dm = (1-energyLoss) * -(2 * m2) / (m1 + m2) * LA.dot(rv1, rp1) / d / d;
-	const v2dm = (1-energyLoss) * -(2 * m1) / (m1 + m2) * LA.dot(rv2, rp2) / d / d;
-
-	const v1d = LA.mult(v1dm, rp1);
-	const v2d = LA.mult(v2dm, rp2);
-
-	c1.vx = v1d.x;
-	c1.vy = v1d.y;
-	c2.vx = v2d.x;
-	c2.vy = v2d.y;
 }
 
-const checkCollisions = (i) => {
+const checkCollisions = (i, t) => {
 
 	const c1 = circles[i];
+	const s = 1/eulerIterations;
 	for(let j=0; j<circles.length; j++) {
 		const c2 = circles[j];
 		if(i !== j && LA.distance(c1, c2) < c1.r+c2.r) {
-			undoStep(c1);
-			undoStep(c2);
-			handleCollision(c1, c2);
-			step(c1);
-			step(c2);
+			undoStep(c1, s);
+			undoStep(c2, s);
+			handleCollision2(c1, c2);
+			step(c1, s);
+			step(c2, s);
 		}
 	}
 
 	if(c1.x + c1.r > canvasWidth || c1.x - c1.r < 0) {
-		undoStep(c1);
+		undoStep(c1, s);
 		c1.vx *= -1+friction;
-		step(c1);
+		step(c1, s);
 	}
 	if(c1.y + c1.r > canvasHeight || c1.y - c1.r < 0) {
-		undoStep(c1);
+		undoStep(c1, s);
 		c1.vy *= -1+friction;
-		step(c1);
+		step(c1, s);
 	}
+	 
 }
 
 const drawLoop = () => {
@@ -191,10 +172,12 @@ const interval = setInterval(() => {
 	for(let i=0; i<circles.length; i++) {
 
 		const c = circles[i];
-		c.vy = Math.min(maxVelocity, c.vy + g);
 
-		step(c);
-		checkCollisions(i);
+		
+		for(let t=0; t<eulerIterations; t++) {
+			step(c, 1/eulerIterations);
+			checkCollisions(i);
+		}
 
 	}
 
