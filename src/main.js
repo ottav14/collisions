1885866460275580e0938cb34aee3d6ec4b2b394
ottav14 <dominicjottaviano@gmail.com>
2025-03-12 02:49:02 -0400

    Energy.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import './style.css'
+import './style.css';
 
 const canvasWidth = 500;
 const canvasHeight = canvasWidth;
 const circleCount = 5;
 const spawnAttempts = 500;
 const circleSizeFactor = 30;
-const minMass = 1;
-const maxMass = 5;
-const g = 0.3;
-const maxVelocity = 7;
-const initialVelocity = maxVelocity;
+const mass = 1;
+const g = 0;
+const energyLoss = 0.3;
+const maxVelocity = 10;
+const initialVelocity = 7;
 const backgroundColor = '#000'
 const friction = 0.00;
 const timestep = 0.02;
@@ -19,6 +19,73 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext("2d");
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
+
+const add = (v, w) => ({ x: v.x + w.x, y: v.y + w.y });
+const sub = (v, w) => ({ x: v.x - w.x, y: v.y - w.y });
+const mult = (c, v) => ({ x: c * v.x, y: c * v.y });
+const clamp = (val, min, max) => Math.max(Math.min(val, max), min);
+const distance = (p1, p2) => Math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2);
+const dot = (v, w) => (v.x * w.x + v.y * w.y);
+const magnitude = (v) => Math.sqrt(v.x**2 + v.y**2);
+const normalize = (v) => {
+	const mag = magnitude(v);
+	return {
+		x: v.x / mag,
+		y: v.y / mag
+	}
+}
+
+const createCircles = () => {
+	const newCircles = [];
+	for(let i=0; i<circleCount; i++) {
+		const radius = mass * circleSizeFactor;
+
+		let position = {
+			x: Math.floor((canvasWidth-2*radius)*Math.random()) + radius,
+			y: Math.floor((canvasHeight-2*radius)*Math.random()) + radius,
+		}
+			
+		for(let i=0; i<spawnAttempts; i++) {
+			for(const other of newCircles) {
+				if(distance(position, other) > radius+other.r)
+					break;
+				else {
+					position = {
+						x: Math.floor((canvasWidth-2*radius)*Math.random()) + radius,
+						y: Math.floor((canvasHeight-2*radius)*Math.random()) + radius,
+					}
+				}
+			}
+		}
+
+		newCircles.push({
+			x: position.x,
+			y: position.y,
+			vx: Math.floor(Math.random()*initialVelocity),
+			vy: Math.floor(Math.random()*initialVelocity),
+			m: mass,
+			r: Math.sqrt(mass) * circleSizeFactor
+		});
+	}
+	return newCircles;
+}
+const circles = createCircles();
+
+const energyDisplay = document.getElementById('energy');
+let energy = 0;
+const updateEnergyDisplay = () => {
+	let totalEnergy = 0;
+	for(const c of circles) {
+		const v = magnitude({
+			x: c.vx,
+			y: c.vy,
+		});
+		totalEnergy += 0.5 * mass * v * v;
+	}
+	energyDisplay.textContent = `Energy: ${totalEnergy}`;
+}
+
+
 
 const drawCircle = (x, y, r) => {
 	ctx.beginPath();
@@ -33,24 +100,6 @@ const drawLine = (x1, y1, x2, y2) => {
 	ctx.lineTo(x2, y2);
 	ctx.stroke();
 }
-
-const distance = (p1, p2) => Math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2);
-
-const dot = (v, w) => (v.x * w.x + v.y * w.y);
-
-
-const magnitude = (v) => Math.sqrt(v.x**2 + v.y**2);
-
-const normalize = (v) => {
-	const mag = magnitude(v);
-	return {
-		x: v.x / mag,
-		y: v.y / mag
-	}
-}
-
-const add = (v, w) => ({ x: v.x + w.x, y: v.y + w.y });
-const multiply = (c, v) => ({ x: c * v.x, y: c * v.y });
 
 const step = (p) => {
 	p.x += p.vx;
@@ -84,98 +133,54 @@ const hue = (r, g, b) => {
 }
 
 
-const createCircles = () => {
-	const newCircles = [];
-	for(let i=0; i<circleCount; i++) {
-		const mass = Math.floor((maxMass-minMass) * Math.random() + minMass);
-		const radius = mass * circleSizeFactor;
-
-		let position = {
-			x: Math.floor((canvasWidth-2*radius)*Math.random()) + radius,
-			y: Math.floor((canvasHeight-2*radius)*Math.random()) + radius,
-		}
-			
-		for(let i=0; i<spawnAttempts; i++) {
-			for(const other of newCircles) {
-				if(distance(position, other) > radius+other.r)
-					break;
-				else {
-					position = {
-						x: Math.floor((canvasWidth-2*radius)*Math.random()) + radius,
-						y: Math.floor((canvasHeight-2*radius)*Math.random()) + radius,
-					}
-				}
-			}
-		}
-
-		newCircles.push({
-			x: position.x,
-			y: position.y,
-			vx: initialVelocity,
-			vy: 0,
-			m: mass,
-			r: Math.sqrt(mass) * circleSizeFactor
-		});
-	}
-	console.log(newCircles);
-	return newCircles;
-}
-
-
-const circles = createCircles();
 
 
 const handleCollision = (c1, c2) => {
-
-	if(!c1.vx || !c1.vy || !c2.vx || !c2.vy)
-		return;
-
-
-	const m1 = c1.m;
-	const m2 = c2.m;
 	const v1 = { x: c1.vx, y: c1.vy };
 	const v2 = { x: c2.vx, y: c2.vy };
-	const un = normalize(v1);
-	const ut = { x: un.y, y: -un.x };
+	const m1 = c1.m;
+	const m2 = c2.m;
+	const rv1 = sub(v1, v2);
+	const rv2 = sub(v2, v1);
+	const rp1 = sub(c1, c2);
+	const rp2 = sub(c2, c1);
+	const d = distance(c1, c2);
 
-	const v1n = dot(v1, un);
-	const v1t = dot(v1, ut);
-	const v2n = dot(v2, un);
-	const v2t = dot(v2, ut);
+	const v1dm = (1-energyLoss) * -(2 * m2) / (m1 + m2) * dot(rv1, rp1) / d / d;
+	const v2dm = (1-energyLoss) * -(2 * m1) / (m1 + m2) * dot(rv2, rp2) / d / d;
 
-	const v1nd = (v1n * (m1 - m2) + 2 * m2 * v2n) / (m1 + m2);
-	const v2nd = (v2n * (m2 - m1) + 2 * m1 * v1n) / (m1 + m2);
-
-	const v1nf = multiply(v1nd, un);
-	const v1tf = multiply(v1t, ut);
-	const v2nf = multiply(v2nd, un);
-	const v2tf = multiply(v2t, ut);
-
-	const v1d = add(v1nf, v1tf);
-	const v2d = add(v2nf, v2tf);
-
-	const val = 20;
+	const v1d = mult(v1dm, rp1);
+	const v2d = mult(v2dm, rp2);
 
 	c1.vx = v1d.x;
 	c1.vy = v1d.y;
 	c2.vx = v2d.x;
 	c2.vy = v2d.y;
-
 }
 
-const checkCollisions = () => {
-	for(let i=0; i<circles.length; i++) {
-		for(let j=0; j<circles.length; j++) {
-			const c1 = circles[i];
-			const c2 = circles[j];
-			if(i !== j && distance(c1, c2) < c1.r+c2.r) {
-				undoStep(c1);
-				undoStep(c2);
-				handleCollision(c1, c2);
-				step(c1);
-				step(c2);
-			}
+const checkCollisions = (i) => {
+
+	const c1 = circles[i];
+	for(let j=0; j<circles.length; j++) {
+		const c2 = circles[j];
+		if(i !== j && distance(c1, c2) < c1.r+c2.r) {
+			undoStep(c1);
+			undoStep(c2);
+			handleCollision(c1, c2);
+			step(c1);
+			step(c2);
 		}
+	}
+
+	if(c1.x + c1.r > canvasWidth || c1.x - c1.r < 0) {
+		undoStep(c1);
+		c1.vx *= -1+friction;
+		step(c1);
+	}
+	if(c1.y + c1.r > canvasHeight || c1.y - c1.r < 0) {
+		undoStep(c1);
+		c1.vy *= -1+friction;
+		step(c1);
 	}
 }
 
@@ -206,26 +211,18 @@ const drawLoop = () => {
 
 const interval = setInterval(() => {
 	
-	for(const circle of circles) {
-		circle.vy = Math.min(maxVelocity, circle.vy + g);
+	for(let i=0; i<circles.length; i++) {
 
-		circle.x += circle.vx;
-		circle.y += circle.vy;
+		const c = circles[i];
+		c.vy = Math.min(maxVelocity, c.vy + g);
 
-		if(circle.x + circle.r > canvasWidth || circle.x - circle.r < 0) {
-			undoStep(circle);
-			circle.vx *= -1+friction;
-			step(circle);
-		}
-		if(circle.y + circle.r > canvasHeight || circle.y - circle.r < 0) {
-			undoStep(circle);
-			circle.vy *= -1+friction;
-			step(circle);
-		}
-
-		checkCollisions();
+		step(c);
+		checkCollisions(i);
 
 	}
+
+	updateEnergyDisplay();
+
 
 	drawLoop();
 }, 1000*timestep);
